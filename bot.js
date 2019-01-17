13a4ba634c1f7a65ce7ef4eb2ec0b052f6ff1f41
mazenssa <46714246+mazenssa@users.noreply.github.com>
2019-01-17 19:56:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "1";
+const prefix = ".";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | ProBot 1`,"http://twitch.tv/Death Shop")
+client.user.setGame(`.play | ProBot 1`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
